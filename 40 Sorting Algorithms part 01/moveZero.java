025b40e9d9219c628f5384da4d3b783bf81f5b1c
AkshayPrajaCode <akshaykumarprajapati@hotmail.com>
2024-03-07 23:22:28 +0530

    problem fixed

--- 40 Sorting Algorithms part 01/moveZero.java
+++ 40 Sorting Algorithms part 01/moveZero.java
@@ -1,26 +1,23 @@
 public class moveZero {
-
-   public static void main(String[] args) {
-    int [] nums ={45,35,0,87,76};
-    int n = nums.length;
-
-
-    for(int i=0; i<n-1;i++){
-        for(int j= 0; j<n-i-1;j++){
-            if(nums[j]==0 && nums[j+1]!=0){
-                //swap the element like  this: nums[i]=nums[j]; nums[j]=nums[i];
-                int temp = nums[i];
-                nums[i] = nums[j+1];
-                nums[j+1]=temp;
-                }
+    public static void main(String[] args) {
+        int[] nums = {45, 35, 0, 87, 76,95,345,0,345,234,34,0,0};
+        moveZeros(nums);
+        for (int val : nums) {
+            System.out.print(val + " ");
         }
-     }
-            
-    for(int val : nums){
-        System.out.print(val + " ");
     }
 
-   }
-
-
+    public static void moveZeros(int[] nums) {
+        int index = 0; // Index to track the position to place non-zero elements
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                // If the current element is non-zero, move it to the current index and increment index
+                nums[index++] = nums[i];
+            }
+        }
+        // Fill the remaining positions with zeros
+        while (index < nums.length) {
+            nums[index++] = 0;
+        }
+    }
 }
